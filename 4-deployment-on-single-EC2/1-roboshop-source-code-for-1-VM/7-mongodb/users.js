0fee8188d2d55d5def0a62f3ff4572269007010d
sivaram <sivaramadabala@gmail.com>
2025-12-04 01:01:42 +0530

    updated source code and ansible playbooks

---
+++ 4-deployment-on-single-EC2/1-roboshop-source-code-for-1-VM/7-mongodb/users.js
@@ -0,0 +1,17 @@
+//
+// Products
+//
+db = db.getSiblingDB('users');
+db.users.insertMany([
+    {name: 'user', password: 'password', email: 'user@me.com'},
+    {name: 'stan', password: 'bigbrain', email: 'stan@instana.com'},
+    {name: 'phoenix', password: 'phoenix', email: 'phoenix@test.com'},
+    {name: 'partner-57', password: 'worktogether', email: 'howdy@partner.com'}
+]);
+
+// unique index on the name
+db.users.createIndex(
+    {name: 1},
+    {unique: true}
+);
+
